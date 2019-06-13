825799f0ae5942e7fc724ec7d0a885e965ce135c
nasyrov289 <43364535+nasyrov289@users.noreply.github.com>
2019-06-14 00:13:37 +0600

    Add files via upload

---
+++ Java/MediumLevel/Java_Inheritance_II.java
@@ -0,0 +1,13 @@
+class Arithmetic{
+    public String getName(){
+    {
+        return "Arithmetic";
+    }
+}
+class Adder extands Arithmetic
+{
+    public int add(int a, int b)
+    {
+	return a+b;
+    }
+}
