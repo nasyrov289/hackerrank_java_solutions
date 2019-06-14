2c6903ff62aa9777f4e0d8cda65cc3238fa86be3
nasyrov289 <43364535+nasyrov289@users.noreply.github.com>
2019-06-14 13:56:12 +0600

    Update Loops.kt

--- Kotlin/EasyLevel/Loops.kt
+++ Kotlin/EasyLevel/Loops.kt
@@ -1,1 +1,11 @@
+import java.util.*
+import kotlin.text.*
 
+fun main(args: Array<String>) {
+    val scan = Scanner(System.`in`)
+
+    val n = scan.nextLine().trim().toInt()
+    for(i in 0..10){
+        println("$n x $i = ${n*i}")
+    }
+}
