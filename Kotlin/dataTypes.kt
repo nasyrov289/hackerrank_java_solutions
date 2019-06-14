e41f5b34d51935b95e48b9a281c6a9c19e6a3da1
nasyrov289 <43364535+nasyrov289@users.noreply.github.com>
2019-06-14 11:13:57 +0600

    Add files via upload

---
+++ Kotlin/dataTypes.kt
@@ -0,0 +1,35 @@
+import java.io.*
+import java.math.*
+import java.security.*
+import java.text.*
+import java.util.*
+import java.util.concurrent.*
+import java.util.function.*
+import java.util.regex.*
+import java.util.stream.*
+import kotlin.collections.*
+import kotlin.comparisons.*
+import kotlin.io.*
+import kotlin.jvm.*
+import kotlin.jvm.functions.*
+import kotlin.jvm.internal.*
+import kotlin.ranges.*
+import kotlin.sequences.*
+import kotlin.text.*
+
+
+fun main(args: Array<String>) {
+    val scan = Scanner(System.`in`)
+
+    val i: Int = 4
+    val d: Double = 4.0
+    val s: String = "HackerRank "
+
+    var i2 = readLine()!!.toInt()
+    var d2 = readLine()!!.toDouble()
+    var s2 = readLine()!!.toString()
+
+    println(i + i2)
+    println(d + d2)
+    println(s + s2)
+}
