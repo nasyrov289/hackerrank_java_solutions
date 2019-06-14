0edfec88adfd05091ef9fe17b8645237fef67bfc
nasyrov289 <43364535+nasyrov289@users.noreply.github.com>
2019-06-14 11:06:59 +0600

    Update Day_0_Hello_World.kt

--- Kotlin/Day_0_Hello_World.kt
+++ Kotlin/Day_0_Hello_World.kt
@@ -2,7 +2,7 @@ import java.io.*
 import java.util.*
 
 fun main(args: Array<String>) {
-    var inputString = readLine();
-    println("Hello, World.");
-    println(inputString);
+    var inputString = readLine()
+    println("Hello, World.")
+    println(inputString)
 }
