4ddbd765437b36a825f1741b514d6356973e35a0
nasyrov289 <43364535+nasyrov289@users.noreply.github.com>
2019-06-14 11:42:29 +0600

    Create Operators.kt

---
+++ Kotlin/Operators.kt
@@ -0,0 +1,39 @@
+import java.io.*
+import java.math.*
+import java.security.*
+import java.text.*
+import java.util.*
+import java.util.concurrent.*
+import java.util.function.*
+import java.util.regex.*
+import java.util.stream.*
+import kotlin.collections.*
+import kotlin.comparisons.*
+import kotlin.io.*
+import kotlin.jvm.*
+import kotlin.jvm.functions.*
+import kotlin.jvm.internal.*
+import kotlin.ranges.*
+import kotlin.sequences.*
+import kotlin.text.*
+
+fun solve(meal_cost: Double, tip_percent: Int, tax_percent: Int): Unit {
+    var tip = meal_cost * tip_percent/100
+    var tax = meal_cost * tax_percent/100
+    var totalCost = tip + tax + meal_cost
+    println(totalCost.toInt())
+    
+
+}
+
+fun main(args: Array<String>) {
+    val scan = Scanner(System.`in`)
+
+    val meal_cost = scan.nextLine().trim().toDouble()
+
+    val tip_percent = scan.nextLine().trim().toInt()
+
+    val tax_percent = scan.nextLine().trim().toInt()
+
+    solve(meal_cost, tip_percent, tax_percent)
+}
